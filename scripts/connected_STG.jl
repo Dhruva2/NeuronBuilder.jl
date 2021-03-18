155cf0f8e256e27ad17eeb1a98a52c2d885ba1db
Dhruva2 <dhruvaraman@hotmail.com>
2021-03-18 11:54:05 +0000

    change in conductances for STG

--- scripts/connected_STG.jl
+++ scripts/connected_STG.jl
@@ -6,9 +6,7 @@ using ModelingToolkit, OrdinaryDiffEq, Plots
 # LP1_channels = [NaV(100.), CaS(2.), CaT(0.), H(2.5), Ka(2.), KCa(0.), Kdr(5.), Leak(0.03)]
 
 AB1_channels = [NaV(100.), CaS(6.), CaT(2.5), H(0.01), Ka(50.), KCa(5.), Kdr(100.), Leak(0.)]
-
 LP1_channels = [NaV(100.), CaS(4.), CaT(0.), H(0.05), Ka(20.), KCa(0.), Kdr(25.), Leak(0.03)]
-
 PY1_channels = [NaV(100.), CaS(2.), CaT(2.4), H(0.05), Ka(50.), KCa(0.), Kdr(125.), Leak(0.01)]
 
 
@@ -30,6 +28,23 @@ LPAB_glut = Glut(30.)
 # PYLP_glut = Glut(3.)
 # LPAB_glut = Glut(3.)
 
+ABLP_chol = Chol(0.03)
+ABPY_chol = Chol(0.003)
+ABLP_glut = Glut(0.03)
+ABPY_glut = Glut(0.01)
+LPPY_glut = Glut(0.001)
+PYLP_glut = Glut(0.03)
+LPAB_glut = Glut(0.03)
+
+
+# ABLP_chol = Chol(3.)
+# ABPY_chol = Chol(0.01)
+# ABLP_glut = Glut(0.01)
+# ABPY_glut = Glut(1.)
+# LPPY_glut = Glut(0.1)
+# PYLP_glut = Glut(0.1)
+# LPAB_glut = Glut(10.)
+
 AB1 = build_neuron(AB1_channels; name=:AB1, num_inputs = 1)
 PY1 = build_neuron(PY1_channels; name = :PY1, num_inputs = 3)
 LP1 = build_neuron(LP1_channels; name = :LP1, num_inputs = 3)
@@ -47,7 +62,7 @@ gr = add_connection(gr, AB1, PY1, ABPY_glut; i=2)
 gr = add_connection(gr, AB1, PY1, ABPY_chol; i=3)
 
 
-tspan = (0., 5000.)
+tspan = (0., 1000.)
 final_sys = structural_simplify(gr)
 
 prob = ODEProblem(final_sys, 
