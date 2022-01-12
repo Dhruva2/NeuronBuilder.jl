d88f37f9333253357e464d899bbdb8cd075580e4
AndreaRamirezH <83847313+AndreaRamirezH@users.noreply.github.com>
2022-01-12 09:54:54 +0000

    Added conductance regulation factory function

--- src/regulation.jl
+++ src/regulation.jl
@@ -1,9 +1,7 @@
-struct Regulated end 
-
-struct regu_dyn
-    conductances::AbstractVector
-    A::Matrix
-end
+# struct regu_dyn
+#     conductances::AbstractVector
+#     A::Matrix
+# end
 
 abstract type Model end
 struct Zerodominant <: Model end #trait that highlights the regulation mechanism of a type 
@@ -22,22 +20,16 @@ regul_dyn(::Onedominant, xs, Ca) = regul_network(xs, Ca)
 function regul_indep(ch::IonChannel, Ca)
     g = fieldnames(typeof(ch))[1]
     R = fieldnames(typeof(ch))[2]
-    τch = external_params(ch)[2]
-    parameters = @parameters τg Ca_tgt $τch
+    τ_ch = external_params(ch)[2]
+    parameters = @parameters τg Ca_tgt $τ_ch
     states = @variables $g(t) $R(t)
-    conductance,mRna = states[1], states[2]
+    conductance, mRna = states[1], states[2]
+    τch = parameters[3]
     eqs = [D(mRna) ~ (Ca_tgt - Ca) / τch,
         D(conductance) ~ (mRna - conductance) / τg]
     #observed = g ?
     defaultmap = [conductance => getg(ch), mRna => getR(ch)]
-    @named gandR = ODESystem(eqs, t, states, parameters, defaults = defaultmap)
-    return gandR
+    return eqs, states, parameters, defaultmap
 end
 
 #function regul_network(conductances::RegNetwork, Ca)
-
-ionic_current(ion_chan::Channel) = ion_chan.sys.states[end]
-
-calcium_current(IonChannel) = hasCa(ion) ? ionic_current(ch) : Num(0)
-
-#calcium_current()
