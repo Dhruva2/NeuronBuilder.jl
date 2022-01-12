455150857877aa8ac29cb5e7ec82b57d40cb7df7
AndreaRamirezH <83847313+AndreaRamirezH@users.noreply.github.com>
2022-01-12 09:52:05 +0000

    Added `isLeak` function to identify leak channels

--- src/helper_functions.jl
+++ src/helper_functions.jl
@@ -6,14 +6,15 @@ getR(ch) = getfield(ch, fieldnames(typeof(ch))[2])
 """
 return unparameterised type as symbol
 """
-function get_name(ch::Component) 
+function get_name(ch::Component)
     Base.typename(ch |> typeof).name |> Symbol
 end
 
-calcium_current(c::IonChannel,s::ODESystem) = Num(0)
+calcium_current(c::IonChannel, s::ODESystem) = Num(0)
 voltage_hook(V, cs) = V ~ cs.sys.V
 calcium_hook(Ca, cs) = Ca ~ cs.sys.Ca
 
+isLeak(c::IonChannel) = typeof(c) in [NeuronBuilder.Liu.Leak{Float64}, NeuronBuilder.Prinz.Leak{Float64}]
 # """
 # does the channel have a reversal
 # """
