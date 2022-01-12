a735516d3913af5cbcdff281e57eb5241e478154
AndreaRamirezH <83847313+AndreaRamirezH@users.noreply.github.com>
2022-01-12 09:58:20 +0000

    Added regulation taus in `external_params`

--- src/assets/Liu/channels.jl
+++ src/assets/Liu/channels.jl
@@ -9,7 +9,7 @@ end
 
 NaV(x) = NaV(x, x, 0.0, 0.0) #if regulated, R is initialized with the same value as g
 ionic_current(::NaV, sys::ODESystem) = sys.INa
-external_params(::NaV) = (:ENa,)# :τNa)
+external_params(::NaV) = (:ENa, :τNa)
 m∞(::NaV, V) = 1.0 / (1.0 + exp((V + 25.5) / -5.29))
 h∞(::NaV, V) = 1.0 / (1.0 + exp((V + 48.9) / 5.18))
 τm(::NaV, V) = 1.32 - 1.26 / (1 + exp((V + 120.0) / -25.0))
@@ -38,7 +38,7 @@ end
 CaS(x) = CaS(x, x, 20.0, 0.0)
 CaS(x, y) = CaS(x, x, y, 0.0)
 ECa(::CaS, Ca) = (500.0) * (8.6174e-5) * (283.15) * (log(max((3000.0 / Ca), 0.001)))
-external_params(::CaS) = nothing #(:τCaS)
+external_params(::CaS) = (nothing, :τCaS)
 ionic_current(::CaS, sys::ODESystem) = sys.ICaS
 calcium_current(::CaS, sys::ODESystem) = sys.ICaS
 m∞(::CaS, V) = 1.0 / (1.0 + exp((V + 33.0) / -8.1))
@@ -70,7 +70,7 @@ CaT(x) = CaT(x, x, 0.0, 0.0)
 ionic_current(::CaT, sys::ODESystem) = sys.ICaT
 calcium_current(::CaT, sys::ODESystem) = sys.ICaT
 ECa(::CaT, Ca) = (500.0) * (8.6174e-5) * (283.15) * (log(max((3000.0 / Ca), 0.001)))
-external_params(::CaT) = nothing #(:τCaT)
+external_params(::CaT) = (nothing, :τCaT)
 m∞(::CaT, V) = 1.0 / (1.0 + exp((V + 27.1) / -7.2))
 h∞(::CaT, V) = 1.0 / (1.0 + exp((V + 32.1) / 5.5))
 τm(::CaT, V) = 21.7 - 21.3 / (1.0 + exp((V + 68.1) / -20.5));
@@ -98,7 +98,7 @@ end
 
 Ka(x) = Ka(x, x, 0.0, 0.0)
 ionic_current(::Ka, sys::ODESystem) = sys.IKa
-external_params(::Ka) = (:EK,)#  :τKa)
+external_params(::Ka) = (:EK, :τKa)
 m∞(::Ka, V) = 1.0 / (1.0 + exp((V + 27.2) / -8.7))
 h∞(::Ka, V) = 1.0 / (1.0 + exp((V + 56.9) / 4.9))
 τm(::Ka, V) = 11.6 - 10.4 / (1.0 + exp((V + 32.9) / -15.2))
@@ -125,7 +125,7 @@ end
 
 KCa(x) = KCa(x, x, 0.0)
 ionic_current(::KCa, sys::ODESystem) = sys.IKCa
-external_params(::KCa) = (:EK,)# :τKCa)
+external_params(::KCa) = (:EK, :τKCa)
 m∞(::KCa, V, Ca) = (Ca / (Ca + 3.0)) / (1.0 + exp((V + 28.3) / -12.6));
 τm(::KCa, V) = 90.3 - 75.1 / (1.0 + exp((V + 46.0) / -22.7));
 
@@ -148,7 +148,7 @@ end
 
 Kdr(x) = Kdr(x, x, 0.0)
 ionic_current(::Kdr, sys::ODESystem) = sys.IKdr
-external_params(::Kdr) = (:EK,)# :τKdr)
+external_params(::Kdr) = (:EK,:τKdr)
 m∞(::Kdr, V) = 1.0 / (1.0 + exp((V + 12.3) / -11.8));
 τm(::Kdr, V) = 7.2 - 6.4 / (1.0 + exp((V + 28.3) / -19.2));
 
@@ -172,7 +172,7 @@ end
 
 H(x) = H(x, x, 0.0)
 ionic_current(::H, sys::ODESystem) = sys.IH
-external_params(::H) = (:EH,)# :τH)
+external_params(::H) = (:EH, :τH)
 m∞(::H, V) = 1.0 / (1.0 + exp((V + 70.0) / 6.0))
 τm(::H, V) = (272.0 + 1499.0 / (1.0 + exp((V + 42.2) / -8.73)))
 
