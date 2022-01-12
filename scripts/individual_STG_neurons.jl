f7354708cd4a2331ef15caf8e4a424536119aeac
AndreaRamirezH <83847313+AndreaRamirezH@users.noreply.github.com>
2022-01-12 10:00:44 +0000

    Test Liu channels with regulation
    ERROR: LoadError: ExtraEquationsSystemException: The system is unbalanced. There are 12 highest order derivative variables and 14 equations.

--- scripts/individual_STG_neurons.jl
+++ scripts/individual_STG_neurons.jl
@@ -1,22 +1,22 @@
 using NeuronBuilder, OrdinaryDiffEq, ModelingToolkit, Plots
-using NeuronBuilder.Liu
 
 ics = Dict(:V => -60.0, :Ca => 0.05)
-reversals = Dict(:ENa => 50.0, :EH => -20.0, :EK => -80.0, :ELeak => -50.0)
-params = Dict(:Cₘ => 10.0, :τCa => 200.0, :Ca∞ => 0.05, :Ca_tgt => 50.0, :τg => 1.0e6)
-#taus = Dict(:τNa => 1e6, :τCaS => 1e6, :τCaT => 1e6, :τKCa => 1e6, :τKa => 1e6, :τKdr => 1e6, :τH => 1e6)
-comp = Soma(ics, merge(reversals, params), 0)
+reversals = Dict(:ENa => 50.0, :EH => -20.0, :EK => -80.0, :ELeak => -5.0)
+params = Dict(:Cₘ => 1.0, :τCa => 200.0, :Ca∞ => 0.05, :Ca_tgt => 50.0, :τg => 1.0e6, :Iapp => -0.5)
+taus = Dict(:τNa => 1e6, :τCaS => 1e6, :τCaT => 1e6, :τKCa => 1e6, :τKa => 1e6, :τKdr => 1e6, :τH => 1e6)
+compart = Soma(ics, merge(reversals, params, taus), 0)
 
 ##From MyModelMenagerie
 #stg_liu = [Liu.NaV(100.0), Liu.CaS(3.0), Liu.CaT(1.3), Liu.Ka(5.0), Liu.KCa(10.0), Liu.Kdr(20.0), Liu.H(0.5), Liu.Leak(0.01)]
+stg_liu = [Liu.NaV(55.0), Liu.CaS(2.0), Liu.CaT(1.4), Liu.Ka(52.0), Liu.KCa(30.0), Liu.Kdr(45.0), Liu.H(1.0), Liu.Leak(0.01)]
+HH = [Liu.NaV(159.23), Liu.Leak(0.16), Liu.Kdr(47.77)]
+tesis = [Liu.NaV(700.0), Liu.CaS(2.25), Liu.CaT(6.25), Liu.Ka(85.0), Liu.KCa(50.0), Liu.Kdr(90.0), Liu.H(0.03), Liu.Leak(0.01)]
 
-## parameters from Prinz 2003 https://journals.physiology.org/doi/pdf/10.1152/jn.00641.2003
-#burster_channels = [Prinz.NaV(400.0), Prinz.CaS(8.0), Prinz.CaT(0.0), Prinz.H(0.04), Prinz.Ka(50.0), Prinz.KCa(20.0), Prinz.Kdr(50.0), Prinz.Leak(0.0)]
 
 ## parameters from O'Leary
-AB1_channels = [Liu.NaV(1831.0), Liu.CaS(27.0), Liu.CaT(23.0), Liu.H(10.1), Liu.Ka(246.0), Liu.KCa(980.0), Liu.Kdr(610.0), Liu.Leak(0.99)]
+AB1_channels = [Liu.NaV(183.10), Liu.CaS(2.70), Liu.CaT(2.30), Liu.H(1.01), Liu.Ka(24.60), Liu.KCa(98.00), Liu.Kdr(61.00), Liu.Leak(0.099)]
 
-AB = build_neuron(comp, AB1_channels; name = :AB1)
+AB = build_neuron(compart, HH; reg = true, name = :AB1)
 #burster = build_neuron(comp, stg_liu; name = :Burster)
 
 final = structural_simplify(AB)
