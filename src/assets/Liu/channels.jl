935b598e48604fcf9e76a090688c9a024421e3bc
AndreaRamirezH <83847313+AndreaRamirezH@users.noreply.github.com>
2022-01-14 10:29:16 +0000

    added struct RegIon holding IonChannels

--- src/assets/Liu/channels.jl
+++ src/assets/Liu/channels.jl
@@ -1,23 +1,33 @@
-#################### NaV ###############################
+mutable struct RegIon <: RegIonChannel
+    ionch::IonChannel
+end
 
+getR(ch) = getfield(ch, fieldnames(typeof(ch))[2])
+
+#################### NaV ###############################
 mutable struct NaV{S,T} <: IonChannel
-    gNa::S #can be a scalar or a variable
-    RNa::S #if unregulated then this is 0.0
+    gNa::S # scalar or variable
+    RNa::S
     mNa::T
     hNa::T
 end
 
-NaV(x) = NaV(x, x, 0.0, 0.0) #if regulated, R is initialized with the same value as g
+NaV(x; reg = false) = reg ? RegIon(NaV(x, x, 0.0, 0.0)) : NaV(x, 0.0, 0.0, 0.0) #if regulated, R is initialized with the same value as g
 ionic_current(::NaV, sys::ODESystem) = sys.INa
-external_params(::NaV) = (:ENa, :τNa)
+external_params(::NaV) = (:ENa, :τNa, :Ca_tgt, :τg)
 m∞(::NaV, V) = 1.0 / (1.0 + exp((V + 25.5) / -5.29))
 h∞(::NaV, V) = 1.0 / (1.0 + exp((V + 48.9) / 5.18))
 τm(::NaV, V) = 1.32 - 1.26 / (1 + exp((V + 120.0) / -25.0))
 τh(::NaV, V) = (0.67 / (1.0 + exp((V + 62.9) / -10.0))) * (1.5 + 1.0 / (1.0 + exp((V + 34.9) / 3.6)))
 
 function channel_dynamics(ch::NaV, V, Ca)
-    states = @variables mNa(t) hNa(t) INa(t)
-    parameters = @parameters gNa ENa
+    if getR(ch) == 0.0
+        states = @variables mNa(t) hNa(t) INa(t)
+        parameters = @parameters gNa ENa
+    elseif getR(ch) != 0.0
+        states = @variables mNa(t) hNa(t) INa(t) gNa(t)
+        parameters = @parameters ENa
+    end
     eqs = [D(mNa) ~ (1 / τm(ch, V)) * (m∞(ch, V) - mNa),
         D(hNa) ~ (1 / τh(ch, V)) * (h∞(ch, V) - hNa),
         INa ~ gNa * mNa^3 * hNa * (ENa - V)]
@@ -35,10 +45,10 @@ mutable struct CaS{S,T} <: IonChannel
     hCaS::T
 end
 
-CaS(x) = CaS(x, x, 20.0, 0.0)
-CaS(x, y) = CaS(x, x, y, 0.0)
+CaS(x; reg = false) = reg ? RegIon(CaS(x, x, 20.0, 0.0)) : CaS(x, 0.0, 20.0, 0.0)
+CaS(x, y; reg = false) = reg ? RegIon(CaS(x, x, y, 0.0)) : CaS(x, 0.0, y, 0.0)
 ECa(::CaS, Ca) = (500.0) * (8.6174e-5) * (283.15) * (log(max((3000.0 / Ca), 0.001)))
-external_params(::CaS) = (nothing, :τCaS)
+external_params(::CaS) = (nothing, :τCaS, :Ca_tgt, :τg)
 ionic_current(::CaS, sys::ODESystem) = sys.ICaS
 calcium_current(::CaS, sys::ODESystem) = sys.ICaS
 m∞(::CaS, V) = 1.0 / (1.0 + exp((V + 33.0) / -8.1))
@@ -47,8 +57,13 @@ h∞(::CaS, V) = 1.0 / (1.0 + exp((V + 60.0) / 6.2))
 τh(::CaS, V) = 60.0 + 150.0 / (exp((V + 55.0) / 9.0) + exp((V + 65.0) / -16.0))
 
 function channel_dynamics(ch::CaS, V, Ca)
-    states = @variables mCaS(t) hCaS(t) ICaS(t)
-    parameters = @parameters gCaS
+    if getR(ch) == 0.0
+        states = @variables mCaS(t) hCaS(t) ICaS(t)
+        parameters = @parameters gCaS
+    elseif getR(ch) == 0.0
+        states = @variables mCaS(t) hCaS(t) ICaS(t) gCaS(t)
+        parameters = ()
+    end
     eqs = [D(mCaS) ~ (1 / τm(ch, V)) * (m∞(ch, V) - mCaS),
         D(hCaS) ~ (1 / τh(ch, V)) * (h∞(ch, V) - hCaS),
         ICaS ~ gCaS * mCaS^3 * hCaS * (ECa(ch, Ca) - V)]
@@ -66,19 +81,24 @@ mutable struct CaT{S,T} <: IonChannel
     hCaT::T
 end
 
-CaT(x) = CaT(x, x, 0.0, 0.0)
+CaT(x; reg = false) = reg ? RegIon(CaT(x, x, 0.0, 0.0)) : CaT(x, 0.0, 0.0, 0.0)
 ionic_current(::CaT, sys::ODESystem) = sys.ICaT
 calcium_current(::CaT, sys::ODESystem) = sys.ICaT
 ECa(::CaT, Ca) = (500.0) * (8.6174e-5) * (283.15) * (log(max((3000.0 / Ca), 0.001)))
-external_params(::CaT) = (nothing, :τCaT)
+external_params(::CaT) = (nothing, :τCaT, :Ca_tgt, :τg)
 m∞(::CaT, V) = 1.0 / (1.0 + exp((V + 27.1) / -7.2))
 h∞(::CaT, V) = 1.0 / (1.0 + exp((V + 32.1) / 5.5))
 τm(::CaT, V) = 21.7 - 21.3 / (1.0 + exp((V + 68.1) / -20.5));
 τh(::CaT, V) = 105.0 - 89.8 / (1.0 + exp((V + 55.0) / -16.9));
 
 function channel_dynamics(ch::CaT, V, Ca)
-    states = @variables mCaT(t) hCaT(t) ICaT(t)
-    parameters = @parameters gCaT
+    if getR(ch) == 0.0
+        states = @variables mCaT(t) hCaT(t) ICaT(t)
+        parameters = @parameters gCaT
+    elseif getR(ch) != 0.0
+        states = @variables mCaT(t) hCaT(t) ICaT(t) gCaT(t)
+        parameters = @parameters ()
+    end
     eqs = [D(mCaT) ~ (1 / τm(ch, V)) * (m∞(ch, V) - mCaT),
         D(hCaT) ~ (1 / τh(ch, V)) * (h∞(ch, V) - hCaT),
         ICaT ~ gCaT * mCaT^3 * hCaT * (ECa(ch, Ca) - V)]
@@ -96,17 +116,22 @@ mutable struct Ka{S,T} <: IonChannel
     hKa::T
 end
 
-Ka(x) = Ka(x, x, 0.0, 0.0)
+Ka(x; reg = false) = reg ? RegIon(Ka(x, x, 0.0, 0.0)) : Ka(x, 0.0, 0.0, 0.0)
 ionic_current(::Ka, sys::ODESystem) = sys.IKa
-external_params(::Ka) = (:EK, :τKa)
+external_params(::Ka) = (:EK, :τKa, :Ca_tgt, :τg)
 m∞(::Ka, V) = 1.0 / (1.0 + exp((V + 27.2) / -8.7))
 h∞(::Ka, V) = 1.0 / (1.0 + exp((V + 56.9) / 4.9))
 τm(::Ka, V) = 11.6 - 10.4 / (1.0 + exp((V + 32.9) / -15.2))
 τh(::Ka, V) = 38.6 - 29.2 / (1.0 + exp((V + 38.9) / -26.5))
 
 function channel_dynamics(ch::Ka, V, Ca)
-    states = @variables mKa(t) hKa(t) IKa(t)
-    parameters = @parameters gKa EK
+    if getR(ch) == 0.0
+        states = @variables mKa(t) hKa(t) IKa(t)
+        parameters = @parameters gKa EK
+    elseif getR(ch) != 0.0
+        states = @variables mKa(t) hKa(t) IKa(t) gKa(t)
+        parameters = @parameters EK
+    end
     eqs = [D(mKa) ~ (1 / τm(ch, V)) * (m∞(ch, V) - mKa),
         D(hKa) ~ (1 / τh(ch, V)) * (h∞(ch, V) - hKa),
         IKa ~ gKa * mKa^3 * hKa * (EK - V)]
@@ -123,15 +148,20 @@ mutable struct KCa{S,T} <: IonChannel
     mKCa::T
 end
 
-KCa(x) = KCa(x, x, 0.0)
+KCa(x; reg = false) = reg ? RegIon(KCa(x, x, 0.0)) : KCa(x, 0.0, 0.0)
 ionic_current(::KCa, sys::ODESystem) = sys.IKCa
-external_params(::KCa) = (:EK, :τKCa)
+external_params(::KCa) = (:EK, :τKCa, :Ca_tgt, :τg)
 m∞(::KCa, V, Ca) = (Ca / (Ca + 3.0)) / (1.0 + exp((V + 28.3) / -12.6));
 τm(::KCa, V) = 90.3 - 75.1 / (1.0 + exp((V + 46.0) / -22.7));
 
 function channel_dynamics(ch::KCa, V, Ca)
-    states = @variables mKCa(t) IKCa(t)
-    parameters = @parameters gKCa EK
+    if getR(ch) == 0.0
+        states = @variables mKCa(t) IKCa(t)
+        parameters = @parameters gKCa EK
+    elseif getR(ch) != 0.0
+        states = @variables mKCa(t) IKCa(t) gKCa(t)
+        parameters = @parameters EK
+    end
     eqs = [D(mKCa) ~ (1 / τm(ch, V)) * (m∞(ch, V, Ca) - mKCa),
         IKCa ~ (gKCa * mKCa^4) * (EK - V)]
     current = [eqs[2]]
@@ -146,15 +176,20 @@ mutable struct Kdr{S,T} <: IonChannel
     mKdr::T
 end
 
-Kdr(x) = Kdr(x, x, 0.0)
+Kdr(x; reg = false) = reg ? RegIon(Kdr(x, x, 0.0)) : Kdr(x, 0.0, 0.0)
 ionic_current(::Kdr, sys::ODESystem) = sys.IKdr
-external_params(::Kdr) = (:EK,:τKdr)
+external_params(::Kdr) = (:EK, :τKdr, :Ca_tgt, :τg)
 m∞(::Kdr, V) = 1.0 / (1.0 + exp((V + 12.3) / -11.8));
 τm(::Kdr, V) = 7.2 - 6.4 / (1.0 + exp((V + 28.3) / -19.2));
 
 function channel_dynamics(ch::Kdr, V, Ca)
-    states = @variables mKdr(t) IKdr(t)
-    parameters = @parameters gKdr EK
+    if getR(ch) == 0.0
+        states = @variables mKdr(t) IKdr(t)
+        parameters = @parameters gKdr EK
+    elseif getR(ch) != 0.0
+        states = @variables mKdr(t) IKdr(t) gKdr(t)
+        parameters = @parameters EK
+    end
     eqs = [D(mKdr) ~ (1 / τm(ch, V)) * (m∞(ch, V) - mKdr),
         IKdr ~ (gKdr * mKdr^4) * (EK - V)]
     current = [eqs[2]]
@@ -170,15 +205,20 @@ mutable struct H{S,T} <: IonChannel
     mH::T
 end
 
-H(x) = H(x, x, 0.0)
+H(x; reg = false) = reg ? RegIon(H(x, x, 0.0)) : H(x, 0.0, 0.0)
 ionic_current(::H, sys::ODESystem) = sys.IH
-external_params(::H) = (:EH, :τH)
+external_params(::H) = (:EH, :τH, :Ca_tgt, :τg)
 m∞(::H, V) = 1.0 / (1.0 + exp((V + 70.0) / 6.0))
 τm(::H, V) = (272.0 + 1499.0 / (1.0 + exp((V + 42.2) / -8.73)))
 
 function channel_dynamics(ch::H, V, Ca)
-    states = @variables mH(t) IH(t)
-    parameters = @parameters gH EH
+    if getR(ch) == 0.0
+        states = @variables mH(t) IH(t)
+        parameters = @parameters gH EH
+    elseif getR(ch) != 0.0
+        states = @variables mH(t) IH(t) gH(t)
+        parameters = @parameters EH
+    end
     eqs = [D(mH) ~ (1 / τm(ch, V)) * (m∞(ch, V) - mH),
         IH ~ gH * mH * (EH - V)]
     current = [eqs[2]]
