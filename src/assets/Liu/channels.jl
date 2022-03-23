5bb2936da51e568945c34348e58bd978acbd25a6
Dhruva Venkita Raman <dhruvaraman@hotmail.com>
2022-03-23 15:49:58 +0000

    changed incorrectly parametrically typed struct (Liu.Kdr)

--- src/assets/Liu/channels.jl
+++ src/assets/Liu/channels.jl
@@ -1,7 +1,7 @@
 
 #################### NaV ###############################
-struct Na{ F<:AbstractFloat} <: IonChannel 
-    gNa::F  
+struct Na{F<:AbstractFloat} <: IonChannel
+    gNa::F
     mNa::F
     hNa::F
 end
@@ -27,13 +27,13 @@ end
 
 #################### Slow calcium current #############################
 
-struct CaS{ F<:AbstractFloat} <: IonChannel
+struct CaS{F<:AbstractFloat} <: IonChannel
     gCaS::F
     mCaS::F
     hCaS::F
 end
 
-CaS(x) = CaS(x, 20.0, 0.0) 
+CaS(x) = CaS(x, 20.0, 0.0)
 ECa(::CaS, Ca) = (500.0) * (8.6174e-5) * (283.15) * (log(max((3000.0 / Ca), 0.001))) #same as conductor 
 external_params(::CaS) = (:τCaS,)
 ionic_current(::CaS, sys::ODESystem) = sys.ICaS
@@ -56,13 +56,13 @@ end
 
 #################### Transient calcium current ######################
 
-struct CaT{ F<:AbstractFloat} <: IonChannel
+struct CaT{F<:AbstractFloat} <: IonChannel
     gCaT::F
     mCaT::F
     hCaT::F
 end
 
-CaT(x) = CaT(x, 0.0, 0.0) 
+CaT(x) = CaT(x, 0.0, 0.0)
 ionic_current(::CaT, sys::ODESystem) = sys.ICaT
 calcium_current(::CaT, sys::ODESystem) = sys.ICaT
 ECa(::CaT, Ca) = (500.0) * (8.6174e-5) * (283.15) * (log(max((3000.0 / Ca), 0.001)))
@@ -85,13 +85,13 @@ end
 
 #################### A-type potassium current #########################
 
-struct Ka{ F<:AbstractFloat} <: IonChannel
+struct Ka{F<:AbstractFloat} <: IonChannel
     gKa::F
     mKa::F
     hKa::F
 end
 
-Ka(x) = Ka(x, 0.0, 0.0) 
+Ka(x) = Ka(x, 0.0, 0.0)
 ionic_current(::Ka, sys::ODESystem) = sys.IKa
 external_params(::Ka) = (:EK, :τKa)
 m∞(::Ka, V) = 1.0 / (1.0 + exp((V + 27.2) / -8.7))
@@ -112,12 +112,12 @@ end
 
 ################### Calcium-activated potassium current ########
 
-struct KCa{ F<:AbstractFloat} <: IonChannel
+struct KCa{F<:AbstractFloat} <: IonChannel
     gKCa::F
     mKCa::F
 end
 
-KCa(x) = KCa(x, 0.0) 
+KCa(x) = KCa(x, 0.0)
 ionic_current(::KCa, sys::ODESystem) = sys.IKCa
 external_params(::KCa) = (:EK, :τKCa)
 m∞(::KCa, V, Ca) = (Ca / (Ca + 3.0)) / (1.0 + exp((V + 28.3) / -12.6));
@@ -134,12 +134,12 @@ function channel_dynamics(ch::KCa, V, Ca)
 end
 
 #################### Delayed rectifier potassium current ######################
-struct Kdr{ F<:AbstractFloat} <: IonChannel
-    gKdr::T
+struct Kdr{F<:AbstractFloat} <: IonChannel
+    gKdr::F
     mKdr::F
 end
 
-Kdr(x) = Kdr(x, 0.0) 
+Kdr(x) = Kdr(x, 0.0)
 ionic_current(::Kdr, sys::ODESystem) = sys.IKdr
 external_params(::Kdr) = (:EK, :τKdr)
 m∞(::Kdr, V) = 1.0 / (1.0 + exp((V + 12.3) / -11.8));
@@ -157,12 +157,12 @@ end
 
 #################### H current ####################
 
-struct H{ F<:AbstractFloat} <: IonChannel
+struct H{F<:AbstractFloat} <: IonChannel
     gH::F
     mH::F
 end
 
-H(x) = H(x, 0.0) 
+H(x) = H(x, 0.0)
 ionic_current(::H, sys::ODESystem) = sys.IH
 external_params(::H) = (:EH, :τH)
 m∞(::H, V) = 1.0 / (1.0 + exp((V + 70.0) / 6.0))
